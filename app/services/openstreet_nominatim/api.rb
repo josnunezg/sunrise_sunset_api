e155d738d8839ee4f3316c298cc724ac5444c57f
Jose Nunez <josnunezg@gmail.com>
2025-05-31 22:11:12 -0400

    add services to get city coords and sunrise and sunset data

--- app/services/openstreet_nominatim/api.rb
+++ app/services/openstreet_nominatim/api.rb
@@ -1,10 +1,20 @@
-module OpenstreetNominatim
-  class Api
-    FORMAT = "json".freeze
-    base_uri SunriseSunset::Config.get("OPENSTREET_NOMINATIM_API_BASE_URL")
+class OpenstreetNominatim::Api
+  FORMAT = "json".freeze
 
-    def search(city:, country:)
-      self.class.get("/search?city=#{city}&country=#{country}&format=#{FORMAT}")
+  def initialize
+    puts Resolv.getaddress(SunriseSunset::Config.get("OPENSTREET_NOMINATIM_API_BASE_URL"))
+    full_url = "https://#{SunriseSunset::Config.get("OPENSTREET_NOMINATIM_API_BASE_URL")}"
+    @conn = Faraday.new(url: full_url, ssl: { verify: Rails.env.production? }) do |faraday|
+      faraday.headers["User-Agent"] = "MiAppRuby/1.0 (contacto@miapp.com)"
+      faraday.headers["Accept"] = "application/json"
+      faraday.options.open_timeout = 10
+      faraday.options.timeout = 10
+      faraday.adapter Faraday.default_adapter
+      faraday.response :json
     end
+  end
+
+  def search(city:, country:)
+    @conn.get("/search?#{URI.encode_www_form({ city: city, country: country, format: FORMAT })}")
   end
 end
--- app/services/sunrise_and_sunset/api.rb
+++ app/services/sunrise_and_sunset/api.rb
@@ -1,22 +1,27 @@
 class SunriseAndSunset::Api
-  include HTTParty
-  base_uri SunriseSunset::Config.get("SUNRISE_SUNSET_API_BASE_URL")
-
   attr_reader :base_query
 
   def initialize(lat:, lng:)
+    puts Resolv.getaddress(SunriseSunset::Config.get("SUNRISE_SUNSET_API_BASE_URL"))
+    full_url = "https://#{SunriseSunset::Config.get("SUNRISE_SUNSET_API_BASE_URL")}"
+    @conn = Faraday.new(url: full_url) do |faraday|
+      faraday.options.open_timeout = 10
+      faraday.options.timeout = 10
+      faraday.adapter Faraday.default_adapter
+      faraday.response :json
+    end
     @base_query = { lat: lat, lng: lng }
   end
 
   def search
-    self.class.get("/json", { query: base_query })
+    @conn.get("/json?#{URI.encode_www_form(base_query)}")
   end
 
   def search_by_day(day:)
-    self.class.get("/json", { query: base_query.merge({ date: day }) })
+    @conn.get("/json?#{URI.encode_www_form(base_query.merge({ date: day }))}")
   end
 
   def search_by_range_of_date(date_start:, date_end:)
-    self.class.get("/json", { query: base_query.merge({ date_start: date_start, date_end: date_end }) })
+    @conn.get("/json?#{URI.encode_www_form(base_query.merge({ date_start: date_start, date_end: date_end }))}")
   end
 end
